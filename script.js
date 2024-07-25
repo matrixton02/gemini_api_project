f1419f1451677834232fe9c2ea0c803f43877b0d
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2024-07-25 15:04:08 +0530

    backend in server half complete

---
+++ script.js
@@ -0,0 +1,24 @@
+let text;
+function gettext(){
+  var text_area = document.getElementById('code');
+  text = text_area.value;
+  console.log(text);
+}
+document.getElementById('submit').onclick = async function() {
+  gettext();
+  try {
+    const response = await fetch('http://localhost:3000/send-text', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: text })
+    });
+
+    const result = await response.text();
+    alert(result);
+    document.getElementById('answer').innerHTML = result;
+  } catch (error) {
+    console.error('Error sending text:', error);
+  }
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const { GoogleGenerativeAI  } = require("@google/generative-ai");
+const dotenv=require("dotenv");
+dotenv.config();
+// console.log(process.env.GEMINI_API_KEY);
+const API_KEY = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+async function run(text) {
+    // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+  
+    const prompt = "\nwhat is the time complexity of the above program in 1 word"
+  
+    const result = await model.generateContent( text + prompt);
+    const response = await result.response;
+    const output = response.text();
+    return output;
+}
+
+// Middleware to parse incoming JSON requests
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/send-text', async (req, res) => {
+    const { text } = req.body;
+    console.log('Received text:', text);
+    const output =await run(text);
+    res.send(" " + output);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
