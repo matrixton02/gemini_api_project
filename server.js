1695d383375d35a421e75c80c622b208befd75ad
Soumyajyoti <145247157+HelloSniperMonkey@users.noreply.github.com>
2024-07-30 17:55:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 const path = require('path');
 
